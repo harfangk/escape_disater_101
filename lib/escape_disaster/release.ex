71663f84db0eb5bba42d45b4323975ee413ba5b0
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-11-22 15:06:08 +0900

    Update migration code in release
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lib/escape_disaster/release.ex
+++ lib/escape_disaster/release.ex
@@ -5,11 +5,16 @@ defmodule EscapeDisaster.Release do
   """
   @app :escape_disaster
 
-  def migrate do
+  @doc """
+   Migrate the database. Defaults to migrating to the latest, `[all: true]`
+   Also accepts `[step: 1]`, or `[to: 20200118045751]`
+  """
+
+  def migrate(opts \\ [all: true]) do
     load_app()
 
     for repo <- repos() do
-      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, opts))
     end
   end
 
@@ -18,11 +23,45 @@ defmodule EscapeDisaster.Release do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
+  def migration_status do
+    for repo <- repos(), do: print_migrations_for(repo)
+  end
+
   defp repos do
     Application.fetch_env!(@app, :ecto_repos)
   end
 
   defp load_app do
     Application.load(@app)
+  end
+
+  defp print_migrations_for(repo) do
+    paths = repo_migrations_path(repo)
+
+    {:ok, repo_status, _} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.migrations(&1, paths), mode: :temporary)
+
+    IO.puts(
+      """
+      Repo: #{inspect(repo)}
+        Status    Migration ID    Migration Name
+      --------------------------------------------------
+      """ <>
+        Enum.map_join(repo_status, "\n", fn {status, number, description} ->
+          "  #{pad(status, 10)}#{pad(number, 16)}#{description}"
+        end) <> "\n"
+    )
+  end
+
+  defp repo_migrations_path(repo) do
+    config = repo.config()
+    priv = config[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
+    config |> Keyword.fetch!(:otp_app) |> Application.app_dir() |> Path.join(priv)
+  end
+
+  defp pad(content, pad) do
+    content
+    |> to_string
+    |> String.pad_trailing(pad)
   end
 end
